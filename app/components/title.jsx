a26c006468a0c3e2fc63ff4a7f0c14a11226b527
Jake Thomas <hey.jthomas@gmail.com>
2024-06-25 11:37:24 +0100

    changed mt for title

--- app/components/title.jsx
+++ app/components/title.jsx
@@ -1,5 +1,5 @@
 const Title = () => (
-  <div className="mx-auto flex justify-center mt-20 mb-40">
+  <div className="mx-auto flex justify-center mt-48 mb-40">
     <img className="mx-auto w-10/12 md:w-8/12" src="title.png" alt="" />
   </div>
 );
